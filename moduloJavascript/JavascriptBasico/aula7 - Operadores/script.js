f9b23ade0bb1c9a2eddd26e4b940244fd3e5d14d
elgyen <julio.filho1@hotmail.com>
2022-06-25 11:12:34 -0300

    Aulas Javascript básico

---
+++ moduloJavascript/JavascriptBasico/aula7 - Operadores/script.js
@@ -0,0 +1,58 @@
+/*
+Adição +
+Subtração -
+Multiplicação *
+Divisão /
+Incremento ++
+Decremento --
+
+Atribuição
++=
+-=
+*/
+
+// Adição (+)
+// NaN = Not a Number
+let a = 10
+let b = 5
+let c = a + b
+console.log(c)
+
+// Subtração
+let d = 10
+let e = 5
+let f = d - e
+console.log(f)
+
+// Multiplicação
+let g = 10
+let h = 5
+let i = g * h
+console.log(i)
+
+// Divisão
+let j = 10
+let k = 5
+let l = j / k
+console.log(l)
+
+// Incremento
+let m = 10
+m++
+console.log(m)
+
+// Decremento
+let n = 10
+n--
+console.log(n)
+
+// atribuição
+let o = 10
+// o = o + 25
+o += 25
+console.log(o)
+
+let r = 10
+//r = r - 3
+r -= 3
+console.log(r)
---
+++ moduloJavascript/JavascriptBasico/aula8 - Operadores Comparativos/script.js
@@ -0,0 +1,51 @@
+/*
+== Igualdade
+=== Igualdade de valores e tipos 
+!== Diferença
+> Maior que
+< Menor que
+>= maior ou igual
+<= menor ou igual
+*/
+
+// Igualdade de valores
+// let a = 10
+// let b = '10'
+// console.log(a == b)
+
+// Igualdade de valores e tipos
+// let a = 10
+// let b = '10'
+// console.log(a === b)
+
+// Diferença de valores
+// let a = 10
+// let b = '10'
+// console.log(a!=b)
+
+// Diferença de valores e tipos
+// let a = 10
+// let b = '10'
+// console.log(a!==b)
+
+// Maior e menor que
+// let a = 10
+// let b = 9
+
+// console.log(a>b)
+// let a = 10
+// let b = 9
+// console.log(a<b)
+
+// let a = 10
+// let b = 10
+// console.log(a>b)
+
+// menor ou igual e maior ou igual
+// let a = 10
+// let b = 10
+// console.log(a>=b)
+
+let a = 10
+let b = 10
+console.log(a<=b)
